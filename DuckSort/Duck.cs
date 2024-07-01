7673e8306189f684d4bdc85e62d7e3071fa61c69
Bartek <bartlomiej.kacki@gmail.com>
2024-07-01 19:32:50 +0200

    Add project files.

---
+++ DuckSort/Duck.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuckSort
+{
+    internal class Duck : IComparable<Duck>
+    {
+        public int Size { get; set; }
+        public KindOfDuck Kind {  get; set; }
+
+        public int CompareTo(Duck? duckToCompare)
+        {
+            if (this.Size > duckToCompare.Size)
+                return 1;
+            else if (this.Size < duckToCompare.Size)
+                return -1;
+            else 
+                return 0;
+        }
+    }
+
+    enum KindOfDuck
+    {
+        krzyżówka,
+        piżmowa,
+        nur,
+    }
+
+    enum SortCriteria
+    {
+        SizeThenKind,
+        KindThenSize,
+    }
+
+    class DuckComparer : IComparer<Duck>
+    {
+        public SortCriteria SortBy = SortCriteria.SizeThenKind;
+
+        public int Compare(Duck? x, Duck? y)
+        {
+            if(SortBy == SortCriteria.SizeThenKind)
+            {
+                if (x.Size > y.Size)
+                    return 1;
+                else if (x.Size < y.Size)
+                    return -1;
+                else 
+                {
+                    if (x.Kind > y.Kind)
+                        return 1;
+                    else if (x.Kind < y.Kind)
+                        return -1;
+                    else
+                        return 0;
+                }
+            }
+            else
+            {
+                if (x.Kind > y.Kind)
+                    return 1;
+                else if (x.Kind < y.Kind)
+                    return -1;
+                else
+                {
+                    if (x.Size > y.Size)
+                        return 1;
+                    else if (x.Size < y.Size)
+                        return -1;
+                    else
+                        return 0;
+                }
+            }
+        }
+    }
+}
---
+++ DuckSort/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace DuckSort
+{
+    internal class Program
+    {
+        public static void PrintDucks(List<Duck> ducks)
+        {
+            foreach (Duck duck in ducks)
+                Console.WriteLine($"{duck.Kind}, {duck.Size} cm");
+        }
+
+        static void Main(string[] args)
+        {
+            List<Duck> ducks = new List<Duck>()
+            {
+                new Duck() { Kind = KindOfDuck.krzyżówka, Size=43},
+                new Duck() { Kind = KindOfDuck.piżmowa, Size=45 },
+                new Duck() { Kind = KindOfDuck.nur, Size=35 },
+                new Duck() { Kind = KindOfDuck.piżmowa, Size=27 },
+                new Duck() { Kind = KindOfDuck.krzyżówka, Size=35 },
+                new Duck() { Kind = KindOfDuck.nur, Size=33 },
+            };
+
+            DuckComparer comparer = new DuckComparer();
+
+            Console.WriteLine("\nSortowanie według gatunku, potem według wielkości:\n");
+            comparer.SortBy = SortCriteria.KindThenSize;
+            ducks.Sort(comparer);
+            PrintDucks(ducks);
+
+            Console.WriteLine("\nSortowanie według wielkości, potem według gatunku:\n");
+            comparer.SortBy = SortCriteria.SizeThenKind;
+            ducks.Sort(comparer);
+            PrintDucks(ducks);
+        }
+    }
+}
